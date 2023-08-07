2b8f56387d794e71a0d117be43c06705a3337f46
Benedikt Strehle <benedikt.strehle@uni-ulm.de>
2023-08-07 14:45:01 +0200

    add formula file

---
+++ Formula/uix.rb
@@ -0,0 +1,12 @@
+
+class UIX < Formula
+	desc "UIX Fullstack Web App Framework"
+	homepage "https://unyt.org/uix"
+	url "https://dev.cdn.unyt.org/uix/run.sh"
+	version "0.0.1"
+  
+	def install
+	  bin.install "run.sh"
+	end
+  end
+  
