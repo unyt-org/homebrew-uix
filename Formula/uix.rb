c0b41164ba6b9c43051716d13801b89755547873
Benedikt Strehle <benedikt.strehle@uni-ulm.de>
2023-11-17 17:59:53 +0100

    Update to stable CDN installation

--- Formula/uix.rb
+++ Formula/uix.rb
@@ -2,8 +2,8 @@
 class Uix < Formula
 	desc "UIX Fullstack Web App Framework"
 	homepage "https://unyt.org/uix"
-	url "https://dev.cdn.unyt.org/uix/run.sh"
-	version "0.0.1"
+	url "https://cdn.unyt.org/uix/run.sh"
+	version "0.1.6"
   
 	def install
 	  bin.install "run.sh" => "uix"
