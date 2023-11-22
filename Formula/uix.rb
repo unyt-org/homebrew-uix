6a3306c3889a6de284650f667b94ae3affc89397
Benedikt Strehle <benedikt.strehle@uni-ulm.de>
2023-11-22 21:57:35 +0100

    Update uix.rb

--- Formula/uix.rb
+++ Formula/uix.rb
@@ -2,8 +2,8 @@
 class Uix < Formula
 	desc "UIX Fullstack Web App Framework"
 	homepage "https://unyt.org/uix"
-	url "https://cdn.unyt.org/uix@0.1.9/run.sh"
-	version "0.1.9"
+	url "https://cdn.unyt.org/uix@0.1.15/run.sh"
+	version "0.1.15"
   
 	def install
 	  bin.install "run.sh" => "uix"
